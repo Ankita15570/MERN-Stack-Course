675a25fbb4aa9620d13726aabc15712a8cf94cc7
Ankita15570 <anshupatil1901@gmail.com>
2025-06-07 20:45:51 +0530

    done

--- 2 Month/javaScript/1index.js
+++ 2 Month/javaScript/1index.js
@@ -1,1 +1,25 @@
-console.log("My Name Is Ankita Patil");
+//Variables
+
+let Name = "Ankita";
+const SecondName = "Anshu";
+var surName = "Patil";
+
+//Functions
+
+function addNumbers(a,b) {
+    console.log(a+b);
+}
+
+//Arrys
+
+const firstArray= [1,3,5,7]
+
+//Objects
+
+const firstObjecct = {
+    name: "Ankita"
+ }
+
+ //String
+
+ const firstString = "Ankita Patil";
