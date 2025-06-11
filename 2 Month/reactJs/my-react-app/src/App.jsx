b93e1f51b4cd4d67aa62018f8e07a561c5c12146
Ankita15570 <anshupatil1901@gmail.com>
2025-06-11 19:47:22 +0530

    done

---
+++ 2 Month/reactJs/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Ankita Jaywant Patil.</h1>
+      <p>At.Post:-Virvade,Tal:-Karad,Dist:-Satara</p>
+    </>
+  )
+}
+
+export default App
