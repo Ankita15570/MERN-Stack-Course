1de4152b8275bd0476fad2c745c8e6e69eb7ce25
Ankita15570 <anshupatil1901@gmail.com>
2025-06-13 20:43:26 +0530

    IQ-Done1

---
+++ 2 Month/reactJs/firstdemo/src/App.jsx
@@ -0,0 +1,36 @@
+
+import { useState } from "react";
+
+
+
+
+function App() {
+  const [name, setName] = useState(); //Hooks
+
+  const [showName, setShowName] = useState(false);
+
+  const handleOnChange = (e) => {
+    console.log(e.target.value);
+    setName(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setShowName(true);
+  };
+
+
+  return (
+    <>
+      <input
+        name="name"
+        placeholder="Enter Your Name"
+        onChange={handleOnChange}
+      />
+
+      <button onClick={handleSubmit}>Submit</button>
+      {showName && <h1>{name}</h1>}
+    </>
+  );
+}
+
+export default App
